74815b5e713be669fa9fecc2cfa3dc33e6b99969
Florentin DUBOIS <dubois.florentin@live.fr>
2015-11-19 10:34:05 +0100

    update

--- confs/arch.php
+++ confs/arch.php
@@ -8,32 +8,26 @@
     // ------------------------------------------------------------------------
     define( 'DIR_CONFIGURATIONS', 'confs' );
     define( 'DIR_CONTROLLERS', 'controllers' );
-    define( 'DIR_CORE', 'core' );
-        define( 'DIR_CORE_CLASSES', DIR_CORE.'/classes' );
-        define( 'DIR_CORE_FUNCTIONS', DIR_CORE.'/functions' );
-        define( 'DIR_CORE_INTERFACES', DIR_CORE.'/interfaces' );
-
     define( 'DIR_MODELS', 'models' );
         define( 'DIR_CLASSES', DIR_MODELS.'/classes' );
         define( 'DIR_FUNCTIONS', DIR_MODELS.'/functions' );
         define( 'DIR_INTERFACES', DIR_MODELS.'/interfaces' );
 
-        define( 'DIR_HTML', DIR_MODELS.'/html.dis' );
-            define( 'DIR_TEMPLATES', DIR_HTML.'/templates' );
-            define( 'DIR_LAYOUTS', DIR_HTML.'/layouts' );
 
     define( 'DIR_PACKAGES', 'packages' );
     define( 'DIR_ASSETS', 'assets' );
             define( 'DIR_LESS', DIR_ASSETS.'/less' );
             define( 'DIR_SCSS', DIR_ASSETS.'/scss');
             define( 'DIR_SASS', DIR_ASSETS.'/sass' );
-            define( 'DIR_LIVESCRIPT', DIR_ASSETS.'/ls' );
+            define( 'DIR_ECMASCRIPT', DIR_ASSETS.'/es6' );
             define( 'DIR_COFFEESCRIPT', DIR_ASSETS.'/coffee' );
             define( 'DIR_ASSETS_BUILD', DIR_ASSETS.'/build' );
                 define( 'DIR_ASSETS_CSS', DIR_ASSETS_BUILD.'/css' );
                 define( 'DIR_ASSETS_JS', DIR_ASSETS_BUILD.'/js' );
 
     define( 'DIR_VIEWS', 'views' );
+        define( 'DIR_TEMPLATES', DIR_VIEWS.'/templates' );
+        define( 'DIR_LAYOUTS', DIR_VIEWS.'/layouts' );
         define( 'DIR_LANGUAGES', DIR_VIEWS.'/languages' );
         define( 'DIR_RESSOURCES', DIR_VIEWS.'/ressources' );
             define( 'DIR_CSS', DIR_RESSOURCES.'/css' );
