059d17276aa7e8e0ea60652d121dd234f5cd1617
Florentin DUBOIS <dubois.florentin@live.fr>
2016-01-29 14:32:53 +0100

    update

--- views/languages/fr-FR/portfolio.php
+++ views/languages/fr-FR/portfolio.php
@@ -105,7 +105,7 @@
     define( 'DESC_MONITEUR_OVH', 'Projet ayant pour but de monitorer son ou ses serveurs ovh (kimsufi, ...) via l\'api node de ovh. Le but est d\'avoir un client lourd qui requète l\'api de ovh. Pour pouvoir mettre en place ce client, on utilise la technologie <a href="http://electron.atom.io/" target="_blank">electron</a> créée pour le projet <a href="https://atom.io" target="_blank" >atom</a>. Il est disponible <a href="https://github.com/FlorentinDUBOIS/ovh-monitor">ici</a>.' );
 
     define( 'MENTIONS_LEGALES_HEADING', 'Mentions légales' );
-    define( 'MENTIONS_LEGALES_CONTENT', 'Copyright (c) 2015(s), Florentin DUBOIS<br /><br />
+    define( 'MENTIONS_LEGALES_CONTENT', 'Copyright (c) 2016(s), Florentin DUBOIS<br /><br />
                                          Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.<br /><br />
                                          THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.' );
     define( 'MENTIONS_LEGALES_OK', 'Fermer' );
