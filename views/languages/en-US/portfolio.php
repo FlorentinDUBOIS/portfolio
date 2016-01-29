f9085247550a87b914c7c1bb1f7c8b178ef612e0
Florentin DUBOIS <dubois.florentin@live.fr>
2016-01-29 13:16:27 +0100

    update

--- views/languages/en-US/portfolio.php
+++ views/languages/en-US/portfolio.php
@@ -22,45 +22,45 @@
     define( 'LICENCE', 'Mentions légales' );
     define( 'GOOD_VISIT', 'Je vous souhaite une bonne visite sur mon portfolio.<br />N\'hésitez pas à me laisser un message' );
     define( 'COPYRIGHT', '© '.date( 'Y' ).' Copyright Florentin DUBOIS ' );
+    define( 'ME', 'Bonjour ! Je m\'appelle Florentin DUBOIS, je suis actuellement étudiant à <a href="http://www.isen.fr" >l\'ISEN Brest</a>, école qui me permet d\'étudier <b>une de mes passions l\'informatique</b>.<br><br>J\'ai découvert ce domaine merveilleux entre la première et le terminal et depuis je ne cesse de <b>m\'intéresser aux nouvelles technologies</b>. <b>J\'adore découvrir de nouveaux langages et framework par moi-même</b>, même si généralement je les découvres grâce à mes amis.<br><br>Je suis un grand fan du C, C++ et javascript pour <b>créer des applications web ou logiciel</b>. Je me considère comme un <b>développeur touche à tout</b> qui peux intervenir sur le projet à tout moment ou bien lors du déploiment. Personnellement je m\'oriente vers un serveur avec <a href="https://archlinux.org" >Arch Linux</a> ou <a href="https://coreos.com" >CoreOS</a> utilisant <a href="https://www.docker.com" >Docker</a>. <br><br>N\'ayez pas peur de m\'envoyer un message par <a href="https://twitter.com" >Twitter</a> ou directement par mon <a data-href="#contact" >portfolio</a>.' );
     define( 'PORTFOLIO_TITLE', 'Florentin DUBOIS - Portfolio' );
-    define( 'ME', 'Bonjour ! Je m\'appelle Florentin DUBOIS, je suis actuellement étudiant à l\'ISEN Brest, école qui me permet d\'étudier une de mes passions l\'informatique.<br>J\'ai' );
 
-    define( 'C_CPP', 'Ces deux langages sont ceux que je préfère avec le JavaScript, Ce sont aussi les premiers langages que j\'ai appris' );
-    define( 'QT', 'Une magnifique bibliothèque multi-platforme que j\'utilise pour mes projets ayant besoin d\'une IHM' );
-    define( 'SFML', 'Une autre bibliothèque multi-platforme que j\'utilise cette fois-ci avec la bibliothèque OpenGL' );
-    define( 'OPENGL', 'Une bibliothèque multi-platforme graphique qui me permet de faire plein de triangles ^^' );
-    define( 'JAVA', 'Langage sur lequel, j\'ai appris les design patterns notamment ceux du livre Gang ofonjour ! je m\'appelle Florentin DUBOIS. <br><br> je suis étudiant en ingénierie à l\'ISEN Brest, je souhaite me spécialiser dans le domaine de l\'informatique, j\'ai Four design patterns' );
-    define( 'ASSEMBLY', 'J\'ai fait de l\'assembleur pour une carte ST7 à l\'ISEN' );
+    define( 'C_CPP', 'Le C et le C++ sont deux languages qui me tienne à coeur. Ce sont les premiers langages informatiques que j\'ai appris. A ce jour, ce sont aussi ce que je préfère' );
+    define( 'QT', 'Bibliothèque que j\'ai découvert lors de mon apprentissage du C++, elle me permet de faire facilement des interfaces graphiques.' );
+    define( 'SFML', 'Une seconde bibliothèque qui quant à elle me permet de gérer plus facilement mes contextes pour l\'OpenGL' );
+    define( 'OPENGL', 'OpenGL est une bibliothèque graphique qui me permet de gérer un espace 2D ou 3D' );
+    define( 'JAVA', 'Langage sur lequel, j\'ai appris les design patterns notamment ceux du livre "Gang of Four design patterns". Je l\'utilise pour développer un "Candy Crush" Like dans le cadre de la formation de l\'ISEN Brest' );
+    define( 'ASSEMBLY', 'Dans le cadre de la formation ISEN Brest, j\'ai reçu des cours d\'assembleur pour piloter un carte ST7' );
     define( 'PYTHON', 'Découvert en cours de phyique, pour modéliser les principes de la magnétostatique' );
 
-    define( 'HTML', 'La base de n\'importe qu\'elle site web' );
+    define( 'HTML', 'Peu de chose à dire, la base de tout les sites web' );
     define( 'CSS', 'Sans le monde serai moin beau' );
     define( 'SASS', 'Langage de préprocessing vraiment très pratique et utile, personnellement je préfère son petit frère SCSS' );
-    define( 'LESS', 'Langage de préprocessing intérréssant concurrant de SASS' );
+    define( 'LESS', 'Langage de préprocessing intérréssant concurant de SASS' );
     define( 'JAVASCRIPT', 'Un de mes langage préféré vraiment très pratique avec des concepts très intérréssant' );
     define( 'COFFEESCRIPT', 'Pour ce simplifier la vie, cependant ECMAScript 2015 comprends la plupart des apports de ce langage de préprocessing' );
     define( 'ANGULAR', 'Un framework JavaScript qui me passionne' );
     define( 'JQUERY', 'Un autre framework JavaScript incontournable' );
     define( 'NODE', 'L\'environnement JavaScript le plus connu' );
-    define( 'EXPRESS', 'Un framework JavaScript pour Node' );
+    define( 'EXPRESS', 'Un framework JavaScript pour Node, qui permet de créer des routes intuitivements avec beaucoup de fonctionnnalité pouvant être ajouté' );
     define( 'JADE', 'Un langage de préprocessing html vraiment très pratique' );
-    define( 'GULP', 'Gestionnaire de tâches géniale' );
+    define( 'GULP', 'Gestionnaire de tâches géniale, basée sur les flux' );
     define( 'APACHE', 'Un vieux de la vieille' );
     define( 'PHP', 'Langage de préprocessing html très pratique, j\'affectionne beaucoup la version 7' );
     define( 'MATERIALIZE', 'Framework css basée sur le style material design que je trouve magnifique' );
     define( 'FOUNDATION', 'Framework css géniale comme base à un nouveau projet' );
-    define( 'BOOTSTRAP', 'Plus besoin de le présenter ^^' );
+    define( 'BOOTSTRAP', 'Plus besoin de le présenter, framework css crée par twitter.' );
 
     define( 'ARCHLINUX', 'Ma distribution Linux préféré elle m\'offre toujours les nouveautés dés que possible (rolling release) <br /><br />Elle suit aussi un de mes principes "KISS" (Keep It Simple, Stupid)' );
-    define( 'DEBIAN', '' );
-    define( 'UBUNTU', 'Comment passé à côté la distribution Linux la plus connu' );
-    define( 'FEDORA', '' );
+    define( 'DEBIAN', 'Distribution que j\'ai utilisé, et c\'est la distribution mère de ubuntu' );
+    define( 'UBUNTU', 'Comment passé à côté la distribution Linux la plus connu. C\'est aussi ma première distribution Linux' );
+    define( 'FEDORA', 'Distribution que j\'ai testé supporté par redhat.' );
     define( 'WINDOWS', 'Dure de ne pas connaître, le système d\'exploitation le plus utilisé sur les ordinateurs' );
 
     define( 'MONGODB', 'MongoDB est un projet NoSQL, auquelle je me suis intérréssé lorsque je découvrit l\'environnement Node.' );
     define( 'MYSQL', 'Principal Base de données utilisé, notamment pour sa gratuité' );
-    define( 'MARIADB', 'Base de données sur laquel je fais la plupart de mes projets descendante de MySQL' );
-    define( 'GIT', 'Gestionnaire de version bien utile pour retracer tout ce qui est fait, utilisé en adéquation avec <a href="http://github.com/FlorentinDUBOIS" >github</a>' );
+    define( 'MARIADB', 'Base de données sur laquel je fais la plupart de mes projets.' );
+    define( 'GIT', 'Gestionnaire de version bien utile pour retracer tout ce qui est fait, utilisé en adéquation avec <a href="http://github.com/FlorentinDUBOIS" target="_blank" >GitHub</a>' );
 
     define( 'NAME_SANDWICH', 'Alternance à aC3' );
     define( 'DATE_SANDWICH', '1 octobre 2014 au 30 septembre 2015' );
@@ -70,30 +70,39 @@
     define( 'DESC_STAGE', 'Stage dit "technicien" dans le cadre de la formation ISEN. <br /><br />Ce stage a durée tout l\'été 2014, dans l\'entreprise aC3 dans le service s\'occupant de la maintenance et le développement de nouvelles fonctionnalités du logiciel' );
 
     define( 'NAME_ENGINEER', 'Diplôme d\'ingénieur' );
-    define( 'DATE_ENGINEER', 'Prévu courant 2017' );
-    define( 'DESC_ENGINEER', 'Diplôme qui me sera délivré en octobre 2017, ce sera un diplôme d\'ingénieur généraliste mais je me suis spécialiser (choix de la majeur) dans le domaine de l\'informatique, choix qui suit une continuité via la formation Cycle informatiques et Réseaux (CIR) de l\'ISEN Brest qui à précédé' );
+    define( 'DATE_ENGINEER', 'Aujourd\'hui - Octobre 2015' );
+    define( 'DESC_ENGINEER', 'Fin de mes études à l\'ISEN Brest prévu pour octobre 2017, j\'ai hâte ^^, je serai alors dôté d\'un diplôme d\'ingénieur généraliste. Cependant je me spécialise vers le domaine de l\'informatique. Pour commencer par le Cycle Informatique et Réseau puis par ma mmajeur Génie Logiciel ainsi que par les différents projets que j\'accomplie.' );
     define( 'NAME_CQPM', 'Certificat de Qualification Paritaire de la Métallurgie' );
     define( 'DATE_CQPM', 'Septembre 2015' );
-    define( 'DESC_CQPM', 'Certificat délivré par l\'organisme "Union des Industries et Métiers de la Métallurgie" (abrégé UIMM) en fin du Cycle Informatiques et Réseaux avec l\'intitulée "Chargé de projets informatiques et réseaux"' );
+    define( 'DESC_CQPM', 'Certificat délivré par l\'organisme "Union des Industries et Métiers de la Métallurgie" (abrégé UIMM) en fin du Cycle Informatiques et Réseaux avec l\'intitulée "Chargé de projets informatiques et réseaux". Ce diplôme à été obtenu à la fin de mes trois ans de Cycle Informatique et Réseau qui m\'a permis de valider mon alternance.' );
     define( 'NAME_CISCO', 'CISCO' );
     define( 'DATE_CISCO', 'Septembre 2013 - âout 2014' );
-    define( 'DESC_CISCO', 'J\'ai suivi les cours des modules 1 et 2, j\'ai aussi suivi différents cours sur les principaux concepts des modules 3 et 4' );
+    define( 'DESC_CISCO', 'J\'ai suivi les cours proposé par le site de l\'entreprise <a href="https://www.netacad.com" >Cisco</a> via la formation Cycle Informatique et Réseau. Les différents cours donnée par cisco sont disposées en 4 modules. J\'ai suivi entièrement les deux premiers et vu les concepts impotants des deux derniers' );
     define( 'NAME_BAC', 'Baccalauréat scientifique' );
     define( 'DATE_BAC', 'Juillet 2012' );
     define( 'DESC_BAC', 'Baccalauréat scientifique obtenue, avec l\'option science de l\'ingénieur et la spécialité mathématiques' );
+    define( 'NAME_CIR', 'Cycle Informatique et Réseau' );
+    define( 'DATE_CIR', 'Septembre 2012 - Septembre 2015' );
+    define( 'DESC_CIR', 'Le Cycle Informatique et Réseau de l\'ISEN Brest est une prépa MPSI avec des cours d\'informatique et de réseaux. Ces cours traitent de différents aspects de ces domaines. Tout d\'abord, j\'ai appris ou continué a me perfectionner dans différents langages, mais c\'est surtout d\'un point de vu théorique que cela m\'a beaucoup apporté par les différents concepts qui existent (liste chainée, arbre, design pattern, ...). De plus ces trois années m\'on permis de découvrir le monde du web grâce à la création de site web puis dans un second temps par les yeux de cisco.' );
 
     define( 'NAME_FESTIGEEK', 'Festigeek' );
-    define( 'DATE_FESTIGEEK', 'Prévu Décembre 2015' );
-    define( 'DESC_FESTIGEEK', '' );
+    define( 'DATE_FESTIGEEK', 'Janvier 2016' );
+    define( 'DESC_FESTIGEEK', 'Projet communaitaire lié au club électronique de l\'ISEN Brest en partenariat avec le PL Guérin. Ce projet est en faites leur site web. Il est sur GitHub à l\'adresse suivante <a href="https://github.com/Nephylhim/festigeek_2016" target="_blank" >https://github.com/Nephylhim/festigeek_2016</a>' );
     define( 'NAME_PORTFOLIO', 'Portfolio' );
-    define( 'DATE_PORTFOLIO', 'Novembre 2015' );
-    define( 'DESC_PORTFOLIO', '' );
+    define( 'DATE_PORTFOLIO', 'Décembre 2015' );
+    define( 'DESC_PORTFOLIO', 'Ce portofolio qui n\'est pas une template, je l\'ai écrit ^^. Il a pour but de me présenter ainsi ceux sur quoi je travaille mais aussi les technologies que j\'utilise. Il est sur GitHub à cette adresse <a href="https://github.com/FlorentinDUBOIS/portfolio" target="_blank" >https://github.com/FlorentinDUBOIS/portfolio</a>'  );
     define( 'NAME_SHELLCHOOSER', 'ShellChooser' );
     define( 'DATE_SHELLCHOOSER', 'Juillet 2015' );
-    define( 'DESC_SHELLCHOOSER', '' );
+    define( 'DESC_SHELLCHOOSER', 'Projet permettant de choisir son shell sous windows. Il est écrit en C++ / Qt. Il est sur GitHub à cette adresse <a href="https://github.com/FlorentinDUBOIS/ShellChooser" target="_blank" >https://github.com/FlorentinDUBOIS/ShellChooser</a>' );
     define( 'NAME_FRAMEWORK', 'Framework PHP' );
-    define( 'DATE_FRAMEWORK', 'Âout 2014' );
-    define( 'DESC_FRAMEWORK', '' );
+    define( 'DATE_FRAMEWORK', 'Août 2014' );
+    define( 'DESC_FRAMEWORK', 'Ensemble d\'objets et de fonction pour développer rapidement en php. Le projet est sur GitHub à cette adresse <a href="https://github.com/FlorentinDUBOIS/framework-php" target="_blank" >https://github.com/FlorentinDUBOIS/framework-php</a>' );
+    define( 'NAME_FROST', 'Frost ' );
+    define( 'DATE_FROST', 'Juillet 2013' );
+    define( 'DESC_FROST', 'Un "Doom" like en moin bien. je l\'avais écrit en C++ et SFML avec OpenGL, cependant ayant perdu le code je peux pas vous le présenter sur GitHub.' );
+    define( 'NAME_MONITEUR_OVH', 'Moniteur OVH' );
+    define( 'DATE_MONITEUR_OVH', 'Janvier 2016' );
+    define( 'DESC_MONITEUR_OVH', 'Projet ayant pour but de monitorer son ou ses serveurs ovh (kimsufi, ...) via l\'api node de ovh. Le but est d\'avoir un client lourd qui requète l\'api de ovh. Pour pouvoir mettre en place ce client on utilise la technologie <a href="http://electron.atom.io/" target="_blank">electron</a> crée pour le projet <a href="https://atom.io" target="_blank" >atom</a>' );
 
     define( 'MENTIONS_LEGALES_HEADING', 'Mentions légales' );
     define( 'MENTIONS_LEGALES_CONTENT', 'Copyright (c) 2015(s), Florentin DUBOIS<br /><br />
