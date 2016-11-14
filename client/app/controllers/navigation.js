b06a38e7fa079568737138e6882174e6ccf2b65f
Florentin DUBOIS <contact@florentin-dubois.fr>
2016-11-14 17:47:45 +0100

    create portfolio 5th version

--- client/app/controllers/navigation.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------------
-// angular controller
-app.controller( 'navigation', ['$scope', '$http', function( $scope, $http ) {
-    $scope.menu = [];
-
-    $http.get( '/navigation' ).then( function( res ) {
-        $scope.menu = res.data;
-    });
-}]);
