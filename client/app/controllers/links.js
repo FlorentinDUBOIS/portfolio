2299ca66f97532284422c478c60e6f3c4cd9534e
Florentin DUBOIS <duboiflorentin@gmail.com>
2017-03-21 12:28:38 +0100

    scratch(project): Start v7

--- client/app/controllers/links.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------------
-// angular controller
-app.controller( 'links', ['$scope', '$http', function( $scope, $http ) {
-    $scope.items = [];
-
-    $http.get( 'links' ).then( function( res ) {
-        $scope.items = res.data;
-    });
-}]);
